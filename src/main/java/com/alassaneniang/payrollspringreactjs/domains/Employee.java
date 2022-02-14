ac17f3e6151559f3e8225b29289b9e909a51f152
Alassane NIANG <adniang75@gmail.com>
2022-02-14 15:55:41 +0100

    Setting up a Custom UI Controller

--- src/main/java/com/alassaneniang/payrollspringreactjs/domains/Employee.java
+++ src/main/java/com/alassaneniang/payrollspringreactjs/domains/Employee.java
@@ -1,31 +1,46 @@
 package com.alassaneniang.payrollspringreactjs.domains;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 @Entity
-public class Employee extends BaseEntity {
+public class Employee {
 
+    @Id
+    @GeneratedValue
+    @Setter( AccessLevel.NONE )
+    private Long id;
     private String firstName;
     private String lastName;
     private String description;
 
+    public Employee ( String firstName, String lastName, String description ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+    }
+
     @Override
-    public String toString () {
-        return "Employee{" +
-                "id='" + getId() + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", description='" + description + '\'' +
-                '}';
+    public boolean equals ( Object o ) {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+
+        Employee employee = (Employee) o;
+
+        return id.equals( employee.id );
+    }
+
+    @Override
+    public int hashCode () {
+        return id.hashCode();
     }
 
 }
