34d17c58155e152dc489c7ac1141a3466a10b20a
Alassane NIANG <adniang75@gmail.com>
2022-02-14 14:47:01 +0100

    Initial commit

---
+++ src/main/java/com/alassaneniang/payrollspringreactjs/PayrollSpringReactJsApplication.java
@@ -0,0 +1,13 @@
+package com.alassaneniang.payrollspringreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollSpringReactJsApplication {
+
+    public static void main ( String[] args ) {
+        SpringApplication.run( PayrollSpringReactJsApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/alassaneniang/payrollspringreactjs/PayrollSpringReactJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alassaneniang.payrollspringreactjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollSpringReactJsApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
