3bd82b87ac4766afe0fef7a3f029a6cdd23f7932
Alassane NIANG <adniang75@gmail.com>
2022-02-14 15:19:24 +0100

    Adjusting the Root URI

--- src/main/java/com/alassaneniang/payrollspringreactjs/bootstrap/DatabaseLoader.java
+++ src/main/java/com/alassaneniang/payrollspringreactjs/bootstrap/DatabaseLoader.java
@@ -3,9 +3,11 @@ package com.alassaneniang.payrollspringreactjs.bootstrap;
 import com.alassaneniang.payrollspringreactjs.domains.Employee;
 import com.alassaneniang.payrollspringreactjs.repositories.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+@Slf4j( topic = "bootstrap" )
 @RequiredArgsConstructor
 @Component
 public class DatabaseLoader implements CommandLineRunner {
@@ -15,5 +17,9 @@ public class DatabaseLoader implements CommandLineRunner {
     @Override
     public void run ( String... args ) {
         repository.save( new Employee( "Frodo", "Baggins", "ring bearer" ) );
+        repository.save( new Employee( "Bilbo", "Baggins", "burglar" ) );
+        repository.findAll().forEach( employee ->
+                log.info( "Preloaded " + employee ) );
+
     }
 }
--- src/main/java/com/alassaneniang/payrollspringreactjs/domains/Employee.java
+++ src/main/java/com/alassaneniang/payrollspringreactjs/domains/Employee.java
@@ -1,19 +1,31 @@
 package com.alassaneniang.payrollspringreactjs.domains;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
 @Entity
 public class Employee extends BaseEntity {
 
     private String firstName;
     private String lastName;
     private String description;
+
+    @Override
+    public String toString () {
+        return "Employee{" +
+                "id='" + getId() + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 
 }
